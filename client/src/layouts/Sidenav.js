6f0dea9e09a4e7fd8d532a18b949d6ceb93eca6e
rakib@pentabd.com <rakib@pentabd.com>
2020-01-08 11:21:46 +0600

    Requesition

--- client/src/layouts/Sidenav.js
+++ client/src/layouts/Sidenav.js
@@ -27,23 +27,25 @@ class Sidenav extends Component {
     }
 
     renderCategory = () => {
+        const {sideNav} = this.props
+        const {pathname} = this.props.location
         const {userType} = jwt.decode(localStorage.getItem('user')) ? jwt.decode(localStorage.getItem('user')).data : ''
         return(
             <>
                 {sidenav.map((items, index) => {
                     return(
-                        <>
-                        {(userType === 2 && items.id === 1) ? null : <>
-                            <p className={`d-flex justify-content-between align-items-center px-4 f-16px ui-cat-title m-0`} onClick={() => {this.handleToggle(index)}} key={index}><i className={`${items.icon} mr-1`}></i> {items.name} <i className="ml-2 fas fa-caret-down"></i></p>
-                            {items.subCat && <div className='ui-subcategory' style={{height: this.state.catId === index ? 'auto' : 0}}>
-                                {items.categories.map((subItems, key) => (
-                                    <>
-                                        <p className={`m-0 pl-4 ${key===1 && 'ui-subcat-active'}`}><a key={subItems.id} href={subItems.link}><i className={`${subItems.icon} mr-1`}></i> {subItems.name}</a></p>
-                                    </>
-                                ))}
-                            </div>}
-                        </>}
-                        </>
+                        <div key={index + 20}>
+                            {(userType !== 1 && items.id === 1) ? null : <>
+                                <p className={`${sideNav ? 'justify-content-center' : 'justify-content-between'} d-flex align-items-center px-4 f-16px ui-cat-title m-0`} onClick={() => {this.handleToggle(index)}} key={index}><i className={`${items.icon} ${sideNav ? 'f-22px' : 'mr-3'}`}></i> {!sideNav && <>{items.name} <i className="ml-2 fas fa-caret-down"></i></>}</p>
+                                {items.subCat && <div className={`ui-subcategory ${sideNav && 'p-0'}`} style={{height: this.state.catId === index ? 'auto' : 0}}>
+                                    {items.categories.map((subItems, key) => (
+                                        <>
+                                            <p key={key + 10} className={`m-0 pl-4 ${pathname === subItems.link ? 'ui-subcat-active' : 'ui-subcat-hover'}`}><a key={subItems.id} href={subItems.link}><i className={`${subItems.icon} ${sideNav ? 'f-22px' : 'mr-3'}`}></i> {!sideNav && subItems.name}</a></p>
+                                        </>
+                                    ))}
+                                </div>}
+                            </>}
+                        </div>
                     )
                 })}
             </>
@@ -51,34 +53,31 @@ class Sidenav extends Component {
     }
 
     render(){
-        const {pathname} = this.props.location
+        const {sideNav} = this.props
         const {userName} = this.state
         const {image} = jwt.decode(localStorage.getItem('user')) ? jwt.decode(localStorage.getItem('user')).data : ''
 
         return(
             <div className='h-100 ui-sidenav'>
-                <div className='ui-user-details'>
-                    <div>
+                <div className='ui-user-details' style={{justifyContent: sideNav && 'center'}}>
+                    {!sideNav && <div>
                     <img className={'ui-user-avatar'} src={'http://localhost:5000/' + image} alt={'user'} />{userName}
-                    </div>
+                    </div>}
                     <i onClick={this.props.handleSideNav} className="fas fa-bars"></i>
                 </div>
                 <div className='ui-sidenav'>
-                    <small className='ml-4 mb-1 mt-3'>Home</small>
-                    <p className='f-16px px-4 ui-cat-title m-0'><Link to={'/'} ><i className="fas fa-home"></i> Homepage</Link></p>
-
-                    <small className='ml-4 mb-1 mt-3'>Main</small>
+                    {!sideNav && <small className='ml-4 mb-1 mt-3'>Home</small>}
+                    <p className={`f-16px ui-cat-title m-0 ${sideNav ? 'text-center' : 'px-4'}`}><Link to={'/'} ><i className={`fas fa-home ${sideNav ? 'f-22px' : ' mr-3'}`}></i> {!sideNav && 'Homepage'}</Link></p>
+                    {!sideNav && <small className='ml-4 mb-1 mt-3'>Main</small>}
                     {this.renderCategory()}
-                
-                    <small className='ml-4 mb-1 mt-3'>User</small>
-                    <p className='f-16px px-4 ui-cat-title m-0'><i class="mr-2 far fa-address-card"></i> <Link to={'/profile'}>Update Profile</Link></p>
-                    <p className='f-16px px-4 ui-cat-title m-0'><i class="mr-2 fas fa-cog"></i> <Link to={'/pass-reset'}>Change Password </Link></p>
-                    <p onClick={() => {localStorage.removeItem('user')}} className='f-16px px-4 ui-cat-title m-0'><i class="mr-2 fas fa-sign-out-alt"></i> <a href={'/'}>LOG OUT</a></p>
+                    {!sideNav && <small className='ml-4 mb-1 mt-3'>User</small>}
+                    <p className={`f-16px ui-cat-title m-0 ${sideNav ? 'text-center' : 'px-4'}`}> <Link to={'/profile'}> <i className={` far fa-address-card  ${sideNav ? 'f-22px' : ' mr-3'}`}></i>{ !sideNav && 'Update Profile'} </Link></p>
+                    <p className={`f-16px ui-cat-title m-0 ${sideNav ? 'text-center' : 'px-4'}`}> <Link to={'/pass-reset'}> <i className={` fas fa-cog ${sideNav ? 'f-22px' : ' mr-3'}`}></i>{ !sideNav && 'Change Password' } </Link></p>
+                    <p onClick={() => {localStorage.removeItem('user')}} className={`f-16px ui-cat-title m-0 ${sideNav ? 'text-center' : 'px-4'}`}> <a href={'/'}><i className={`fas fa-sign-out-alt ${sideNav ? 'f-22px' : 'mr-3'}`}></i>{ !sideNav && 'LOG OUT' }</a></p>
                 </div>
             </div>
         )
     }
-    
 }
 
 export default withRouter(Sidenav)
