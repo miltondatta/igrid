db0eb6604e26f17b1837bdceaeeaf4f4c926f041
rakib@pentabd.com <rakib@pentabd.com>
2020-01-13 10:56:18 +0600

    Initail

--- client/src/routes/MasterRoute.js
+++ client/src/routes/MasterRoute.js
@@ -109,7 +109,7 @@ class MasterRoute extends Component{
                                     <Route exact path='/location' component={AsyncLocationFinder}/>
                                     <Route exact path='/pass-reset' component={AsyncPasswordReset}/>
                                     <Route exact path='/request-history' component={AsyncRequestHistory}/>
-                                    <Route exact path='/support-hisotry' component={AsyncSupportHistory}/>
+                                    <Route exact path='Ho' component={AsyncSupportHistory}/>
                                     <Route exact path='/delivery-request' component={AsyncDeliveryRequest}/>
                                     <Route exact path='/request-details/:id' component={AsyncRequestDetails}/>
                                     <Route exact path='/branch-requesition' component={AsyncBranchRequesition}/>
