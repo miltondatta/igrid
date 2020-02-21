02bd63170d253c563e7c1aa9ce4f8a20bfa968cf
Rakib_Penta <rakib@pentabd.com>
2020-02-21 12:10:32 +0600

    Commit Before Pull

--- config/db.js
+++
@@ -1,3 +0,0 @@
-const Sequelize = require('sequelize');
-const conString = require('./config').development;
-module.exports = db = new Sequelize(conString);
--- models/document_category.js
+++
@@ -1,8 +0,0 @@
-const db = require('../config/db');
-const Sequelize = require('sequelize');
-
-const document_category = db.define('document_category', {
-    category_name: Sequelize.STRING
-});
-
-module.exports = document_category;
