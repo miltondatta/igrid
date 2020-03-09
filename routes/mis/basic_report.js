6dae9627ce05305b06e31bbee7aa4046db3e241e
Shahadat Hossain <shahadat@clouditbd.com>
2020-03-09 19:45:07 +0600

    update basic report

--- routes/mis/basic_report.js
+++ routes/mis/basic_report.js
@@ -4,8 +4,7 @@ const moment                = require('moment');
 const mis_imported_data     = require('../../models/mis_imported_data')
 const mis_indicatordetail   = require('../../models/mis_indicatordetail')
 const Locations             = require('../../models/locations')
-  
-
+const { getChildLocations, getMasterComponent, getDetailsComponent } = require('../../dbHelper/mis');
 
 const route = express.Router();
 
@@ -26,45 +25,115 @@ route.get('/mis/basic/report/daily', async(req, res) => {
     dateArray.forEach((item) => {
         columnsString += moment(item).format('MMM_Do') + " FLOAT,";    
     });
-    columnsString       =   columnsString.slice(0, -1);
-    let child_locations = [];
-
-    //Get all last child of this location, including this id
-    const [locations, metadata_location] = await db.query(`with recursive cte (id, hierarchy, parent_id) as (
-        SELECT     id,
-                   hierarchy,
-                   parent_id
-        FROM       locations
-        WHERE      id = ${location_id}
-        UNION ALL
-        SELECT     l.id,
-                   l.hierarchy,	
-                   l.parent_id
-        FROM       locations l
-        INNER JOIN cte
-                ON l.parent_id = cte.id			
-      )
-      select id from cte WHERE hierarchy = (SELECT MAX(location_hierarchies.id) FROM location_hierarchies)`);
-
-      locations.forEach((item, index) => {
-          child_locations.push(item.id);
-      });  
-      child_locations   =   child_locations.join(",");
-
+    columnsString       =  columnsString.slice(0, -1);
+    let child_locations =  await getChildLocations(location_id);
     const [results, metadata] = await db.query(`SELECT * FROM
         crosstab ( 
         $$
-        SELECT mis_indicatordetails.indicator_name, data_date, SUM(data_value) as data_value
-        FROM mis_indicatordetails LEFT JOIN mis_imported_datas ON mis_indicatordetails.id = mis_imported_datas.indicatordetails_id 
-        WHERE (data_date BETWEEN '${date_from}' AND '${date_to}') AND location_id IN (${child_locations})  OR data_date IS NULL
-        GROUP BY mis_indicatordetails.indicator_name, data_date order by 1,2 
+        SELECT indicatordetails_id, data_date, SUM(data_value) as data_value
+        FROM mis_imported_datas 
+        WHERE (data_date BETWEEN '${date_from}' AND '${date_to}') AND location_id IN (${child_locations}) 
+        GROUP BY indicatordetails_id, data_date order by 1,2 
         $$
-        ) AS ct ( indicator VARCHAR, ${columnsString})`)
+        ) AS ct ( indicatordetails_id INT, ${columnsString})`);
+
+       //Get Only Master Component
+       let masterComponents     =  await getMasterComponent();     
+       let detailsComponents    =  await getDetailsComponent(); 
+
+       let finalResults = {};
+     /*  masterComponents.forEach((item) => {
+            finalResults.push(item.indicatormaster_name);
+       });
+
+       array['name'][0,1]
+       array['name1'][0,1]
+
+       console.log(finalResults); */
+
         if (results.length > 0) {
-            res.status(200).json(results)
+            results.forEach((item, index) => {
+                let detailsObj = detailsComponents.find(d => d.id === item.indicatordetails_id);
+                let masterObj  = masterComponents.find(m => m.id === detailsObj.indicatormaster_id);
+
+
+
+
+               // item.indicatordetails_id = detailsObj.indicator_name;
+
+               //console.log(masterObj.indicatormaster_name);
+
+               //console.log(finalResults, 64);
+
+               finalResults[masterObj.indicatormaster_name] = []; 
+
+               finalResults[masterObj.indicatormaster_name].push(item);
+
+               //console.log(finalResults[masterObj.indicatormaster_name], "line 66");
+
+                //finalResults[masterObj[['indicatormaster_name']]].push(item); 
+                //console.log(masterObj.indicatormaster_name);
+                //finalResults[0].push(item);
+                //finalResults[masterObj.indicatormaster_name].push(item);
+
+                
+
+             /*   if(detailsComponents.includes(item.indicator)) {
+                    item.group = true;
+                } else {
+                    item.group = false;
+                } */
+            }); 
+            console.log(finalResults);
+            return res.status(200).json(results)
         } else {
-            res.status(200).json({message: "No Data Found"})
+            return res.status(200).json({message: "No Data Found"})
         }
     });
+
+
+    route.get('/mis/basic/report/weekly', async(req, res) => { 
+        let location_id = req.query.location_id;
+        let date_from   = req.query.date_from;
+        let date_to     = req.query.date_to;
+        let weekArray   =   [];
+    
+        var start_date  = moment(date_from);
+        var end_date    = moment(date_to);
+
+
+        console.log(start_date);
+        
+        while (start_date <= end_date) {
+            var weeknumber = moment(start_date, "MM-DD-YYYY").week();
+            weekArray["Week" + weeknumber] = moment(start_date).format('YYYY-MM-DD');
+            start_date = moment(start_date).add(1, 'days');
+        }
+
+    
+        let columnsString = "";
+        weekArray.forEach((item) => {
+            console.log(item);
+            columnsString += moment(item).format('MMM_Do') + " FLOAT,";    
+        });
+        columnsString       =  columnsString.slice(0, -1);
+        console.log(columnsString);
+
+        let child_locations =  await getChildLocations(location_id);
+        const [results, metadata] = await db.query(`SELECT * FROM
+            crosstab ( 
+            $$
+            SELECT mis_indicatordetails.indicator_name, data_date, SUM(data_value) as data_value
+            FROM mis_indicatordetails LEFT JOIN mis_imported_datas ON mis_indicatordetails.id = mis_imported_datas.indicatordetails_id 
+            WHERE (data_date BETWEEN '${date_from}' AND '${date_to}') AND location_id IN (${child_locations})  OR data_date IS NULL
+            GROUP BY mis_indicatordetails.indicator_name, data_date order by 1,2 
+            $$
+            ) AS ct ( indicator VARCHAR, ${columnsString})`)
+            if (results.length > 0) {
+                res.status(200).json(results)
+            } else {
+                res.status(200).json({message: "No Data Found"})
+            }
+        });
 
 module.exports = route
