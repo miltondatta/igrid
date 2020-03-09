e277c469e65572ce78a00c1654ea8011e09a4248
Rakib_Penta <rakib@pentabd.com>
2020-03-09 11:31:53 +0600

    MIS Report

--- client/src/components/MISReportComponent/DailyReportComponent/DailyReportComponent.js
+++ client/src/components/MISReportComponent/DailyReportComponent/DailyReportComponent.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
+import Axios from "axios";
+import {apiUrl} from "../../../utility/constant";
 
 class DailyReportComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hierarchies: []
+        }
+    }
+
+    componentDidMount() {
+        this.getHierarchies()
+    }
+
+    getHierarchies = () => {
+        Axios.get(apiUrl() + 'locHierarchies')
+            .then(res => {
+                this.setState({
+                    hierarchies: res.data
+                })
+            })
+    }
+
     render() {
+        const {hierarchies} = this.state
+        const locations = hierarchies.length > 0 && hierarchies.map(item => {
+            return(
+                <select onChange={this.handleChange} className={'ui-custom-input'}>
+                    <option></option>
+                </select>
+            )
+        })
+
+
         return (
             <div>
-                
+                {locations}
             </div>
         );
     }
