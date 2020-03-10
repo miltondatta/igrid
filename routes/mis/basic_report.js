ae4b67ea4183e49c3608f5e12ece4961a71909ca
Shahadat Hossain <shahadat@clouditbd.com>
2020-03-10 12:24:44 +0600

    basic report weekly done

--- routes/mis/basic_report.js
+++ routes/mis/basic_report.js
@@ -8,6 +8,27 @@ const { getChildLocations, getMasterComponent, getDetailsComponent } = require('
 
 const route = express.Router();
 
+
+const processFinalResult = async (results) => {
+    let masterComponents    = await getMasterComponent();
+    let detailsComponents   = await getDetailsComponent();
+    let finalResults = {};
+    masterComponents.forEach((item) => {
+        finalResults[item.indicatormaster_name] = [];
+    })
+
+    if (results.length > 0) {
+        results.forEach((item, index) => {
+            let detailsObj = detailsComponents.find(d => d.id === item.indicatordetails_id);
+            let masterObj = masterComponents.find(m => m.id === detailsObj.indicatormaster_id);
+            item.indicatordetails_id = detailsObj.indicator_name;
+            finalResults[masterObj.indicatormaster_name].push(item);
+        });
+    }
+    return finalResults;
+};
+
+
 route.get('/mis/basic/report/daily', async(req, res) => { 
     let location_id = req.query.location_id;
     let date_from   = req.query.date_from;
@@ -36,27 +57,12 @@ route.get('/mis/basic/report/daily', async(req, res) => {
         GROUP BY indicatordetails_id, data_date order by 1,2 
         $$
         ) AS ct ( indicatordetails_id INT, ${columnsString})`);
-
-       //Get Only Master Component
-       let masterComponents     =  await getMasterComponent();     
-       let detailsComponents    =  await getDetailsComponent(); 
-
-       let finalResults = {};
-       masterComponents.forEach((item) => {
-        finalResults[item.indicatormaster_name] = [];
-       })     
-
-
+    
         if (results.length > 0) {
-            results.forEach((item, index) => {
-                let detailsObj = detailsComponents.find(d => d.id === item.indicatordetails_id);
-                let masterObj  = masterComponents.find(m => m.id === detailsObj.indicatormaster_id);
-                item.indicatordetails_id = detailsObj.indicator_name;
-                finalResults[masterObj.indicatormaster_name].push(item);
-            }); 
+            let finalResults = await processFinalResult(results);
             return res.status(200).json(finalResults);
         } else {
-            return res.status(200).json({message: "No Data Found"})
+            return res.status(200).json({ message: "No Data Found" })
         }
     });
 
@@ -69,39 +75,38 @@ route.get('/mis/basic/report/daily', async(req, res) => {
     
         var start_date  = moment(date_from);
         var end_date    = moment(date_to);
-
-
-        console.log(start_date);
         
         while (start_date <= end_date) {
-            var weeknumber = moment(start_date, "MM-DD-YYYY").week();
-            weekArray["Week" + weeknumber] = moment(start_date).format('YYYY-MM-DD');
-            start_date = moment(start_date).add(1, 'days');
+            var weeknumber          = moment(start_date, "MM-DD-YYYY").week();
+            weekArray[weeknumber]   = moment(start_date).format('YYYY-MM-DD');
+            start_date              = moment(start_date).add(1, 'days');
         }
-
     
-        let columnsString = "";
-        weekArray.forEach((item) => {
-            console.log(item);
-            columnsString += moment(item).format('MMM_Do') + " FLOAT,";    
+        let columnsString   = "";
+        let dateIn          = "";
+        weekArray.forEach((item, index) => {
+            columnsString += "Week_" + index + " FLOAT,";   
+            dateIn        += "'" + item + "',";     
         });
-        columnsString       =  columnsString.slice(0, -1);
-        console.log(columnsString);
+        columnsString      =  columnsString.slice(0, -1);
+        dateIn             =  dateIn.slice(0, -1);
 
-        let child_locations =  await getChildLocations(location_id);
-        const [results, metadata] = await db.query(`SELECT * FROM
+        let child_locations         =  await getChildLocations(location_id);
+        const [results, metadata]   = await db.query(`SELECT * FROM
             crosstab ( 
             $$
-            SELECT mis_indicatordetails.indicator_name, data_date, SUM(data_value) as data_value
-            FROM mis_indicatordetails LEFT JOIN mis_imported_datas ON mis_indicatordetails.id = mis_imported_datas.indicatordetails_id 
-            WHERE (data_date BETWEEN '${date_from}' AND '${date_to}') AND location_id IN (${child_locations})  OR data_date IS NULL
-            GROUP BY mis_indicatordetails.indicator_name, data_date order by 1,2 
+            SELECT indicatordetails_id, data_date, SUM(data_value) as data_value
+            FROM mis_imported_datas 
+            WHERE data_date IN (${dateIn}) AND location_id IN (${child_locations}) 
+            GROUP BY indicatordetails_id, data_date order by 1,2 
             $$
-            ) AS ct ( indicator VARCHAR, ${columnsString})`)
+            ) AS ct ( indicatordetails_id INT, ${columnsString})`);
+            
             if (results.length > 0) {
-                res.status(200).json(results)
+                let finalResults = await processFinalResult(results);
+                return res.status(200).json(finalResults);
             } else {
-                res.status(200).json({message: "No Data Found"})
+                return res.status(200).json({ message: "No Data Found" })
             }
         });
 
