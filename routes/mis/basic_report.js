60b3f697f632f769b9b8fec20d72304e676bbbe1
Shahadat Hossain <shahadat@clouditbd.com>
2020-03-12 13:09:19 +0600

    update basic report

--- routes/mis/basic_report.js
+++ routes/mis/basic_report.js
@@ -111,49 +111,6 @@ route.get('/mis/basic/report/daily', async(req, res) => {
         });
 
 
-    route.get('/mis/basic/report/weekly', async(req, res) => { 
-        let location_id = req.query.location_id;
-        let date_from   = req.query.date_from;
-        let date_to     = req.query.date_to;
-        let weekArray   =   [];
-    
-        var start_date  = moment(date_from);
-        var end_date    = moment(date_to);
-        
-        while (start_date <= end_date) {
-            var weeknumber          = moment(start_date, "MM-DD-YYYY").week();
-            weekArray[weeknumber]   = moment(start_date).format('YYYY-MM-DD');
-            start_date              = moment(start_date).add(1, 'days');
-        }
-    
-        let columnsString   = "";
-        let dateIn          = "";
-        weekArray.forEach((item, index) => {
-            columnsString += "Week_" + index + " FLOAT,";   
-            dateIn        += "'" + item + "',";     
-        });
-        columnsString      =  columnsString.slice(0, -1);
-        dateIn             =  dateIn.slice(0, -1);
-
-        let child_locations         =  await getChildLocations(location_id);
-        const [results, metadata]   =  await db.query(`SELECT * FROM
-            crosstab ( 
-            $$
-            SELECT indicatordetails_id, data_date, SUM(data_value) as data_value
-            FROM mis_imported_datas 
-            WHERE data_date IN (${dateIn}) AND location_id IN (${child_locations}) 
-            GROUP BY indicatordetails_id, data_date order by 1,2 
-            $$
-            ) AS ct ( indicatordetails_id INT, ${columnsString})`);
-            
-            if (results.length > 0) {
-                let finalResults = await processFinalResult(results);
-                return res.status(200).json(finalResults);
-            } else {
-                return res.status(200).json({ message: "No Data Found" })
-            }
-        });
-
     route.get('/mis/basic/report/monthly', async(req, res) => { 
         let location_id = req.query.location_id;
         let date_from   = req.query.date_from;
