8e1028bcfcb0c7ce4d51972f88a4d486fbcc784d
shuvo.babl <shuvo.babl@gmail.com>
2020-04-05 18:18:37 +0600

    migration problem resolved

--- migrations/20200203112834-create-modules.js
+++ migrations/20200203112834-create-modules.js
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Modules');
+    return queryInterface.dropTable('modules');
   }
 };
