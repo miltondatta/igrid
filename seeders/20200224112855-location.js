7acecb48ff73e48051fa9eb182bc3f8e221872c3
Shahadat Hossain <shahadat@clouditbd.com>
2020-04-27 14:13:12 +0600

    update location

--- seeders/20200224112855-location.js
+++ seeders/20200224112855-location.js
@@ -59,7 +59,7 @@ module.exports = {
           updatedAt: new Date()
         },
         {
-          location_name: 'Brnach 02',
+          location_name: 'Branch 02',
           location_code: '06',
           address: 'Branch Office',
           location_lat: 25.744860,
@@ -70,7 +70,7 @@ module.exports = {
           updatedAt: new Date()
         },
         {
-          location_name: 'Brnach 03',
+          location_name: 'Branch 03',
           location_code: '07',
           address: 'Branch Office',
           location_lat: 22.820000,
@@ -81,7 +81,7 @@ module.exports = {
           updatedAt: new Date()
         },
         {
-          location_name: 'Brnach 04',
+          location_name: 'Branch 04',
           location_code: '08',
           address: 'Branch Office',
           location_lat: 23.643999,
@@ -92,7 +92,7 @@ module.exports = {
           updatedAt: new Date()
         },
         {
-          location_name: 'Brnach 05',
+          location_name: 'Branch 05',
           location_code: '09',
           address: 'Branch Office',
           location_lat: 23.644480,
@@ -103,13 +103,68 @@ module.exports = {
           updatedAt: new Date()
         },
         {
-          location_name: 'Brnach 06',
+          location_name: 'Branch 06',
           location_code: '10',
           address: 'Branch Office',
           location_lat: 22.723406,
           location_long: 89.075127,
           parent_id: 4,
           hierarchy: 5,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          location_name: 'Gazipur',
+          location_code: '04',
+          address: 'Gazipur',
+          location_lat: 25.079583,
+          location_long: 89.178911,
+          parent_id: 2,
+          hierarchy: 3,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          location_name: 'Narayangonj',
+          location_code: '05',
+          address: 'Narayangonj',
+          location_lat: 22.93018,
+          location_long: 90.132603,
+          parent_id: 2,
+          hierarchy: 3,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          location_name: 'Tangail',
+          location_code: '05',
+          address: 'Narayangonj',
+          location_lat: 89.8173298,
+          location_long: 24.2647858,
+          parent_id: 3,
+          hierarchy: 4,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          location_name: 'Nagorpur',
+          location_code: '06',
+          address: 'Nagorpur',
+          location_lat: 23.222831,
+          location_long: 89.389278,
+          parent_id: 3,
+          hierarchy: 4,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          location_name: 'Chittagong',
+          location_code: '03',
+          address: 'Chittagong',
+          location_lat: 22.93018,
+          location_long: 90.132603,
+          parent_id: 1,
+          hierarchy: 2,
           createdAt: new Date(),
           updatedAt: new Date()
         },
