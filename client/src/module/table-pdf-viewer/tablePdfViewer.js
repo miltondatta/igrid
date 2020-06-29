0667bff193d00e6d7be95cfabd92e566b67d937c
Rakib_Penta <rakib@pentabd.com>
2020-06-29 13:39:38 +0600

    Report Table Fixing

--- client/src/module/table-pdf-viewer/tablePdfViewer.js
+++ client/src/module/table-pdf-viewer/tablePdfViewer.js
@@ -4,7 +4,7 @@ import {Document, Page, PDFViewer, StyleSheet, Text, View, Image} from "@react-p
 
 class TablePdfViewer extends Component {
     render() {
-        const {tableData, pdfViewr, reportTitle} = this.props
+        const {tableData, pdfViewr, reportTitle, admin} = this.props
         const styles = StyleSheet.create({
             page: {
                 alignItems: 'center',
@@ -22,20 +22,30 @@ class TablePdfViewer extends Component {
                 alignItems: 'center'
             },
             headerText: {
-                fontSize: 11,
-                width: 45,
+                fontSize: 9,
                 paddingVertical: 2,
                 borderBottomWidth: 1,
-                marginHorizontal: 3,
+                borderRightWidth: 1,
+                borderTopWidth: 1,
+                paddingHorizontal: 5,
                 textTransform: 'capitalize',
+                backgroundColor: '#ccc'
+            },
+            document: {
+                width: 500,
+                overflow: 'scroll'
             },
             bodyText: {
                 color: '#444',
-                fontSize: 10,
-                width: 45,
+                fontSize: 8,
+                minWidth: 45,
+                maxWidth: 45,
                 paddingVertical: 2,
                 borderBottomWidth: 1,
-                marginHorizontal: 3,
+                paddingHorizontal: 5,
+                borderRightWidth: 1,
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
                 borderBottomColor: '#888',
                 textTransform: 'capitalize',
             }
@@ -43,23 +53,33 @@ class TablePdfViewer extends Component {
         let table_headers = tableData.length > 0 && Object.keys(tableData[0]).map((item, index) => {
             return (
                 <>
-                    {item !== 'id' && <Text key={index} style={styles.headerText}>
-                        {item.replace('_', " ")}
+                    {item !== 'id' && <Text break key={index} style={{
+                        ...styles.headerText,
+                        minWidth: (admin ? 580 : 625) / Object.keys(tableData[0]).length,
+                        maxWidth: (admin ? 580 : 625) / Object.keys(tableData[0]).length,
+                        borderLeftWidth: index === (admin ? 0 : 1) ? 1 : null,
+                    }}>
+                        {item.replace('_', " ").split('-').join(' ')}
                     </Text>}
                 </>
             )})
-        let table_body = tableData.length > 0 && tableData.map((items, index) => {
+        let table_body = tableData.length > 0 && tableData.map((items, indexs) => {
             return (
-                <>
-                    <Text style={{...styles.bodyText, width: 30}}>{index + 1}</Text>
+                <View style={styles.section}>
                     {Object.keys(tableData[0]).map((item, index) => (
                         <>
-                            {item !== 'id' && <Text key={index + 10} style={styles.bodyText}>
-                                {items[item]}
+                            {item !== 'id' && <Text break key={index + 10} style={{
+                                ...styles.bodyText,
+                                minWidth: (admin ? 580 : 625) / Object.keys(tableData[0]).length,
+                                maxWidth: (admin ? 580 : 625) / Object.keys(tableData[0]).length,
+                                borderLeftWidth: index === (admin ? 0 : 1) ? 1 : null,
+                            }}>
+                                {items[item] ? typeof items[item] === 'string' ? items[item].split('-').join(' ') : items[item] : 'N/A'}
                             </Text>}
                         </>
                     ))}
-                </>
+
+                </View>
             )})
         return (
             <div className={'ui-pdf w-100'}>
@@ -67,19 +87,16 @@ class TablePdfViewer extends Component {
                     <i className="fas fa-times-circle"></i>
                 </div>
                 <PDFViewer>
-                    <Document>
-                        <Page size="A4" style={styles.page}>
+                    <Document style={styles.document} >
+                        <Page size="A4" style={styles.page} wrap={true}>
                             <View style={styles.sectionTop}>
                                 <Image style={{width: 65, marginBottom: 15}} src={process.env.PUBLIC_URL + '/media/image/logo.png'} />
                                 <Text style={{...styles.header, marginBottom: 25, color: '#0f5ea8'}}>{reportTitle}</Text>
                             </View>
                             <View style={styles.section}>
-                                <Text style={{...styles.headerText, width: 30}}>No</Text>
                                 {table_headers}
                             </View>
-                            <View style={styles.section}>
-                                {table_body}
-                            </View>
+                            {table_body}
                         </Page>
                     </Document>
                 </PDFViewer>
